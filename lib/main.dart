154ba1e217ab0e64503ee3f755394f8e77d6c789
MrDeners <112748231+MrDeners@users.noreply.github.com>
2023-10-31 21:06:15 +0300

    Main page was updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,94 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
   return MaterialApp(
     theme: ThemeData(
+      scaffoldBackgroundColor: Colors.black,
       colorScheme: const ColorScheme.dark()),
-        home: const Scaffold(
-          body: SafeArea(
+        home:  Scaffold(
+          body:  SafeArea(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/Background.png"),
+                fit: BoxFit.cover,
+                ),
+              ),
             child: Row (
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image(image: AssetImage('assets/Naming.png')),
-                    Text("Hello"),
+                    Container(
+                        width: 346,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage (
+                            image: AssetImage('assets/Naming.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 100)),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 245,
+                        height: 111,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/TheoryButton.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 45)),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 245,
+                        height: 111,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/CalculatorButton.png'),
+                            fit: BoxFit.cover,
+                          )
+                        )
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 45)),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 245,
+                        height: 111,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage (
+                            image: AssetImage('assets/TestsButton.png')
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 45)),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 245,
+                        height: 111,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/SettingsButton.png')
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 )
               ]
             ),
             )
 
-          )
+          ))
         );
 
   }
